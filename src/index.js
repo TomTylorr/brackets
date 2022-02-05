b80154527eede29d069c1ff3bc47fe67c4e2118b
TomTylorr <tomtylorr@gmail.com>
2022-02-05 19:27:09 +0300

    feat: varianty one

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [],
+    bracketsPair = {},
+    stack = [];
+
+  bracketsConfig.forEach(index => {
+    openBrackets.push(index[0]);
+    bracketsPair[index[1]] = index[0];
+  })
+
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+      
+
+      if (bracketsPair[currentSymbol] === topElement) {
+      	
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
+
